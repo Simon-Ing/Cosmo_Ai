498340c09bd3e2b2ddc5037b871c5c51db74c2a2
einzz <72155016+einzz@users.noreply.github.com>
2022-01-13 15:25:00 +0100

    works for windows now

--- main.cpp
+++ main.cpp
@@ -3,12 +3,13 @@
 
 using namespace std;
 
-int size = 401;
+int window_size = 401;
 int R_e = 30;
 int R = 40;
-int x_pos = size / 2;
-int y_pos = size / 2;
+int x_pos = window_size / 2;
+int y_pos = window_size / 2;
 double K_l = 1.0;
+
 double K_s = 1.1;
 cv::Mat source;
 cv::Mat image;
@@ -17,7 +18,7 @@ cv::Mat image;
 
 static void update(int, void*){
     R = std::max(R, 1);
-    source = cv::Mat(size, size, CV_8UC1, cv::Scalar(0, 0, 0));
+    source = cv::Mat(window_size, window_size, CV_8UC1, cv::Scalar(0, 0, 0));
 
 //    for(int i = -100; i < 101; i += 100){
 //        cv::circle(source, cv::Point(x_pos + i, y_pos), 0, cv::Scalar(255, 255, 255), R);
@@ -28,24 +29,24 @@ static void update(int, void*){
     int sigma = (R % 2) ? R : R-1;
     cv::GaussianBlur(source, source, cv::Size_<int>(sigma, sigma), sigma);
 
-    image = cv::Mat(size, size, CV_8UC1, cv::Scalar(0, 0, 0));
+    image = cv::Mat(window_size, window_size, CV_8UC1, cv::Scalar(0, 0, 0));
 
     // This is where the magic happens
     for(int i=0; i < source.rows; i++){
         for(int j=0; j <= source.cols; j++){
-            int x = j - size/2;
-            int y = size/2 - i;
+            int x = j - window_size/2;
+            int y = window_size/2 - i;
             double r = sqrt(x*x + y*y);
             double theta = atan2(y, x);
             double frac = (R_e*R_e * r) / (r*r + R*R + 2*r*R*cos(theta));
             double x_ = K_s/K_l*x + frac * (r/R + cos(theta));
             double y_ = K_s/K_l*y + frac * (-sin(theta));
-            int source_row = size/2 - (int)y_;
-            int source_col = (int)x_ + size/2;
+            int source_row = window_size/2 - (int)y_;
+            int source_col = (int)x_ + window_size/2;
 
             // If source position within source image
-            if(max(source_col, source_row) <= size && min(source_col, source_row) >= 0){
-                image.at<uchar>(i, j) = source.at<uchar>(source_row, source_col);
+            if(max(source_col, source_row) < window_size && min(source_col, source_row) >= 0){
+                image.at<uchar>(i, j) =  source.at<uchar>(source_row, source_col);
             }
         }
     }
@@ -68,8 +69,8 @@ static void update(int, void*){
 int main()
 {
     cv::namedWindow("Window", cv::WINDOW_AUTOSIZE);
-    cv::createTrackbar("x pos", "Window", &x_pos, size, update);
-    cv::createTrackbar("y pos", "Window", &y_pos, size, update);
+    cv::createTrackbar("x pos", "Window", &x_pos, window_size, update);
+    cv::createTrackbar("y pos", "Window", &y_pos, window_size, update);
     cv::createTrackbar("Einstein", "Window", &R_e, 100, update);
     cv::createTrackbar("Radius", "Window", &R, 100, update);
     cv::waitKey(0);
