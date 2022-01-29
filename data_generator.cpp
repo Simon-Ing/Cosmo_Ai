36afe128f99c80da9095afd271cb39983c2f8169
Simon-Ing <simon.ing.89@gmail.com>
2022-01-29 15:55:05 +0100

    made a separate data generator that takes command line arguments

--- data_generator.cpp
+++ data_generator.cpp
@@ -12,22 +12,35 @@
 
 using namespace nlohmann;
 
-int window_size = 600;  // Size of source and lens image
-int source_size = window_size / 10;   // size of source "Blob"
-int einsteinR = window_size / 10;
-int xPos = 0;
+int window_size = 0;  // Size of source and lens image
+unsigned long source_size = window_size / 10;   // size of source "Blob"
+unsigned long einsteinR = window_size / 10;
+unsigned long xPos = 0;
 int actualPos;
 cv::Mat apparentSource;
 cv::Mat image;
+std::string name;
 
 
 // **************  GENERATE DATA SETTINGS ****************************
 static const bool dataGenMode = true; // true for generating data
 int iteration_counter = 0;
-int DATAPOINTS_TO_GENERATE = 1000;
+int DATAPOINTS_TO_GENERATE = 0;
 std::fstream fout;
 
 // **********************************************************
+
+void drawGaussian(cv::Mat& img, int& pos) {
+    for (int row = 0; row < window_size; row++) {
+        for (int col = 0; col < window_size; col++) {
+            double x = (1.0 * (col - (pos + window_size / 2.0))) / source_size;
+            double y = (window_size - 1.0 * (row + window_size / 2.0)) / source_size;
+
+            auto val = (uchar)std::round(255 * std::exp(-x * x - y * y));
+            img.at<uchar>(row, col) = val;
+        }
+    }
+}
 
 // Distort the image
 void distort(int thread_begin, int thread_end, int R) {
@@ -49,11 +62,6 @@ void distort(int thread_begin, int thread_end, int R) {
             double x_ = x + frac * (r / R + cos(theta));
             double y_ = y - frac * sin(theta);
 
-            //            // Print some data when evaluating the point at the origin (for debugging)
-            //            if (row == window_size/2 && col == window_size/2){
-            //                std::cout << "x:  " << x << " y: " << y << " R: " << R << " r: " << r << " theta: " << theta << " EinsteinR: " << einsteinR << std::endl;
-            //            }
-
             // Translate to array index
             int row_ = window_size / 2 - (int)round(y_);
             int col_ = window_size / 2 + R + (int)round(x_);
@@ -66,15 +74,13 @@ void distort(int thread_begin, int thread_end, int R) {
     }
 }
 
-
-
 void writeToPngFiles() {
     std::ostringstream filename_path;
     std::ostringstream filename;
     // for this to work, make a folder called "cosmo_data" in same folder as the main executable
 
-    filename << "datapoint" << iteration_counter << ".png";
-    filename_path << "cosmo_data" << "/" << filename.str();
+    filename << name << iteration_counter << ".png";
+    filename_path << name + "_data" << "/" << filename.str();
     iteration_counter++;
     cv::imwrite(filename_path.str(), image);
 
@@ -113,10 +119,53 @@ void writeToJson() {
     std::cout << filename.str() << " generated and saved on drive" << std::endl;
 }
 
-int main() {
+// Split the image into n pieces where n is number of threads available and distort the pieces in parallel
+static void parallel(int R) {
+    unsigned int num_threads = std::thread::hardware_concurrency();
+    std::vector<std::thread> threads_vec;
+    for (int k = 0; k < num_threads; k++) {
+        unsigned int thread_begin = (apparentSource.rows / num_threads) * k;
+        unsigned int thread_end = (apparentSource.rows / num_threads) * (k + 1);
+        std::thread t(distort, thread_begin, thread_end, R);
+        threads_vec.push_back(std::move(t));
+    }
+    for (auto& thread : threads_vec) {
+        thread.join();
+    }
+}
+
+// This function is called each time a slider is updated
+static void update(int, void*) {
+    int apparentPos1, apparentPos2, R;
+    actualPos = (xPos - window_size / 2);
+    apparentPos1 = (int)(actualPos + sqrt(actualPos * actualPos + 4 * einsteinR * einsteinR)) / 2;
+    R = apparentPos1;
+
+    // Make the undistorted image at the apparent position by making a black background and add a gaussian light source
+    apparentSource = cv::Mat(window_size, window_size, CV_8UC1, cv::Scalar(0, 0, 0));
+    drawGaussian(apparentSource, apparentPos1);
+
+    // Make black background to draw the distorted image to
+    image = cv::Mat(window_size, window_size, CV_8UC1, cv::Scalar(0, 0, 0));
+
+    // Run in parallel
+    parallel(R);
+
+    //writeToPngFiles();  //this creates .csv file also
+    writeToPngFiles();
+}
+
+int main(int argc, char *argv[]) {
+
+    DATAPOINTS_TO_GENERATE = atoi(argv[1]);
+    window_size = atoi(argv[2]);
+    name = std::string(argv[3]);
+
+    std::cout << DATAPOINTS_TO_GENERATE << " " << window_size << " " << name << std::endl;
+
     // Generate dataset:
-    //fout.open("cosmo_data/cosmo_data.csv", fout.trunc | fout.in | fout.out);  // opens .csv file
-    //fout << "filename" << "," << "einsteinR" << "," << "source_size" << "," << "xPos" << " \n";  // Writes the first line to .csv file
+    fout.open(name + "_data/" + name + ".csv", fout.trunc | fout.in | fout.out);  // opens .csv file
+    fout << "filename" << "," << "einsteinR" << "," << "source_size" << "," << "xPos" << " \n";  // Writes the first line to .csv file
 
     std::random_device dev;
     std::mt19937 rng(dev());
