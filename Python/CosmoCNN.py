4b26e9e28a0a3f211de40ddc363ddd45a2d39d32
simon <simon.ing.89@gmail.com>
2022-01-29 16:18:49 +0100

    refactored cnn added scheduler

--- Python/CosmoCNN.py
+++ Python/CosmoCNN.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 from torch.utils.data import DataLoader
 import deepshit
 import time
-
+import torch.optim.lr_scheduler as lr_scheduler
 
 def cuda_if_available():
     if torch.cuda.is_available():
@@ -38,26 +38,9 @@ def dataset_from_json():
     test_dataset = deepshit.CosmoDataset(path='test_dataset.json')
     return (DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True),
             DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False))
+ 
 
-
-def train_network(loader, model_, lossfunc_, optimizer_):
-    timer = time.time()
-    n_total_steps = len(loader)
-    for epoch in range(num_epochs):
-        for i, (images, params) in enumerate(loader):
-            images = images.to(device)
-            params = params.to(device)
-            # Forward pass
-            output = model_(images)
-            loss_ = lossfunc_(output, params)
-            # Backward pass
-            optimizer_.zero_grad()
-            loss_.backward()
-            optimizer_.step()
-            print(f'Epoch: {epoch+1} / {num_epochs}\tstep: {i+1} / {n_total_steps}\tloss: {loss_.item():.10f}\ttime: {(time.time() - timer)}')
-
-
-def test_network(loader, model_, lossfunc_):
+def test_network(loader, model_, lossfunc_, print_results=False):
     total_loss = 0
     n_batches = 0
     with torch.no_grad():
@@ -68,15 +51,16 @@ def test_network(loader, model_, lossfunc_):
             loss_ = lossfunc_(output, params)
             total_loss += loss_
             n_batches += 1
-            for i, param in enumerate(params):
-                nice = [round(n) for n in output[i].tolist()]
-                print(f'Correct: {param.tolist()}, \tOutput: {nice}')
+            if print_results:
+                for i, param in enumerate(params):
+                    nice = [round(n) for n in output[i].tolist()]
+                    print(f'Correct: {param.tolist()}, \tOutput: {nice}')
         return total_loss / n_batches
 
 
 num_epochs = 1000
-batch_size = 100
-# learning_rate = 0.001
+batch_size = 300
+learning_rate = 0.01
 
 device = cuda_if_available()
 
@@ -85,31 +69,47 @@ device = cuda_if_available()
 
 train_loader = torch.load("train_loader.pt")
 test_loader = torch.load("test_loader.pt")
-model = deepshit.ConvNet().to(device)
 
 # load_model()
 
-for lr in range(1, 11):
-    learning_rate = lr/10000
-    lossfunc = nn.MSELoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+model = deepshit.ConvNet().to(device)
+lossfunc = nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+scheduler = lr_scheduler.ReduceLROnPlateau(optimizer)    
 
-    loss_before = test_network(test_loader, model, lossfunc)
-    print(f'\nAverage loss over test data before training: {loss_before}\n')
+loss_before = test_network(test_loader, model, lossfunc)
+print(f'\nAverage loss over test data before training: {loss_before}\n')
 
-    try:
-        train_network(train_loader, model, lossfunc, optimizer)
-    except KeyboardInterrupt:
-        print("Keyboard interrupt, exit training loop")
+timer = time.time()
+n_total_steps = len(train_loader)
+prev_loss = 100000000000
+for epoch in range(num_epochs):
+    for i, (images, params) in enumerate(train_loader):
+        images = images.to(device)
+        params = params.to(device)
+        # Forward pass
+        output = model(images)
+        loss = lossfunc(output, params)
+        # Backward pass
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        print(f'Epoch: {epoch+1} / {num_epochs}\tstep: {i+1} / {n_total_steps}\tloss: {loss.item():.10f}\ttime: {(time.time() - timer)}')
+        
+    scheduler.step(loss)
+    if (epoch+1) % 1 == 0:
+        loss = test_network(test_loader, model, lossfunc, False)
+        print(f"\nLoss test data: {loss} lr: {optimizer.state_dict()['param_groups'][0]['lr']}\n")
 
-    loss_train = test_network(train_loader, model, lossfunc)
-    print(f'\nAverage loss over train data after training: {loss_train}\n')
 
-    loss_test = test_network(test_loader, model, lossfunc)
-    print(f'\nAverage loss over test data after training: {loss_test}\n')
+loss_train = test_network(train_loader, model, lossfunc)
+print(f'\nAverage loss over train data after training: {loss_train}\n')
 
-    # save_model()
+loss_test = test_network(test_loader, model, lossfunc)
+print(f'\nAverage loss over test data after training: {loss_test}\n')
 
-    with open("training_params.txt", "a") as outfile:
-        outfile.write(f'Epochs: {num_epochs}\tbatch size: {batch_size}\tlearning rate: {learning_rate}\taverage loss train data: {loss_train:.1f}\taverage loss test data: {loss_test:.1f}\n')
+# save_model()
+
+with open("training_params.txt", "a") as outfile:
+    outfile.write(f'Epochs: {num_epochs}\tbatch size: {batch_size}\tlearning rate: {learning_rate}\taverage loss train data: {loss_train:.1f}\taverage loss test data: {loss_test:.1f}\n')
 
--- Python/deepshit.py
+++ Python/deepshit.py
@@ -9,36 +9,50 @@ import numpy as np
 class ConvNet(nn.Module):
     def __init__(self):
         super(ConvNet, self).__init__()
-        self.conv1 = nn.Conv2d(1, 6, (5, 5))  # one input (colors), six outputs, kernel(filter) of size 5x5
-        self.conv2 = nn.Conv2d(6, 16, (5, 5))
-        self.conv3 = nn.Conv2d(16, 64, (5, 5))
-        self.conv4 = nn.Conv2d(64, 128, (5, 5))
-        self.pool4 = nn.MaxPool2d(4, 4)  # Takes max value from each set of 4x4 pixels
-        self.pool2 = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(128, 128)  # fully connected (normal vanilla layer), inputs: 128, outputs:128
-        self.fc2 = nn.Linear(128, 84)
-        self.fc3 = nn.Linear(84, 3)
+        self.conv1 = nn.Conv2d(1, 4, (5, 5))  # one input (colors), six outputs, kernel(filter) of size 5x5
+        self.conv2 = nn.Conv2d(4, 8, (5, 5))
+        self.conv3 = nn.Conv2d(8, 16, (5, 5))
+        self.conv4 = nn.Conv2d(16, 20, (5, 5))
+        self.conv5 = nn.Conv2d(20, 20, (5, 5))
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(3920, 1000)  # fully connected (normal vanilla layer), inputs: 128, outputs:128
+        self.fc2 = nn.Linear(1000, 300)
+        self.fc3 = nn.Linear(300, 100)
+        self.fc4 = nn.Linear(100, 30)
+        self.fc5 = nn.Linear(30, 3)
 
     def forward(self, x):
+
         x = self.conv1(x)
-        x = func.leaky_relu(x)  # non-linear activation function
-        x = self.pool4(x)
+        x = func.leaky_relu(x)
+        x = self.pool(x)
+
         x = self.conv2(x)
         x = func.leaky_relu(x)
-        x = self.pool4(x)
+        x = self.pool(x)
+
         x = self.conv3(x)
         x = func.leaky_relu(x)
-        x = self.pool4(x)
+        x = self.pool(x)
+
         x = self.conv4(x)
         x = func.leaky_relu(x)
-        x = self.pool4(x).view(-1, 128)
+        x = self.pool(x)
+
+        x = self.conv5(x)
+        x = func.leaky_relu(x)
+        x = self.pool(x).view(-1, 3920)
 
         # After convolving and pooling we end up with a 1D array, we feed this into the neural net
         x = self.fc1(x)
-        x = func.relu(x)
+        x = func.leaky_relu(x)
         x = self.fc2(x)
-        x = func.relu(x)
+        x = func.leaky_relu(x)
         x = self.fc3(x)  # We end up with three values (x position, Einstein Radius and source size
+        x = func.leaky_relu(x)
+        x = self.fc4(x)
+        x = func.leaky_relu(x)
+        x = self.fc5(x)
         return x
 
 
@@ -66,3 +80,21 @@ class CosmoDataset(Dataset):
         return self.n_samples
 
 
+        # x = self.conv1(x)
+        # x = func.leaky_relu(x)  # non-linear activation function
+        # x = self.pool(x)
+        # x = self.conv2(x)
+        # x = func.leaky_relu(x)
+        # x = self.pool(x)
+        # x = self.conv3(x)
+        # x = func.leaky_relu(x)
+        # x = self.pool(x)
+        # x = self.conv4(x)
+        # x = func.leaky_relu(x)
+        # x = self.pool(x)
+        # x = self.conv4(x)
+        # x = func.leaky_relu(x)
+        # x = self.pool(x)#.view(-1, 128)
+        # x = self.conv4(x)
+        # x = func.leaky_relu(x)
+        # x = self.pool(x)#.view(-1, 128)
