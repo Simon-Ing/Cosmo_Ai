4307f516db1403940b724c27bc4870cf586cfc19
Einar Leite Austnes <eaustnes@gmail.com>
2022-05-02 09:39:52 +0200

    trained models

--- Python/best_test_in_the_west.py
+++ Python/best_test_in_the_west.py
@@ -5,18 +5,18 @@ from torch.utils.data import DataLoader
 #load model
 device = cuda_if_available()  # Use cuda if available
 model = AlexNet().to(device)
-#model = torch.nn.DataParallel(model)
-PATH = "Python/Models/73loss_distratio"
+model = torch.nn.DataParallel(model)
+PATH = "Python/Models/autosave_epoch60_500k"
 checkpoint = torch.load(PATH)
 
-#model.load_state_dict(checkpoint['model_state_dict'])
-model.load_state_dict(torch.load(PATH))
-model.eval()
+model.load_state_dict(checkpoint['model_state_dict'])
+#model.load_state_dict(torch.load(PATH))
+#model.eval()
 
 
 #load test data 
 batch_size = 256
-test_dataset = dataset_from_png(n_samples=5000, size=512, folder="test", gen_new=True)
+test_dataset = dataset_from_png(n_samples=5000, size=512, folder="test", gen_new=False)
 test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size)
 
 
