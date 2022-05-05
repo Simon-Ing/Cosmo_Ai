2258e15861a95ab5f26a27424eecbced27958d99
simonrunde <simonrun@hotmail.com>
2022-05-05 10:18:04 +0200

    Added about menu with some sample text

--- CosmoAI_qt/mainwindow.cpp
+++ CosmoAI_qt/mainwindow.cpp
@@ -10,6 +10,7 @@
 #include <QInputDialog>
 #include <QStyleFactory>
 #include <QFileDialog>
+#include <QMessageBox>
 #define PI 3.14159265358979323846
 
 
@@ -704,3 +705,21 @@ void MainWindow::on_termsSpinbox_valueChanged(int arg1)
 //    }
 //    return {ksi1, ksi2};
 //}
+
+void MainWindow::on_actionAbout_triggered()
+{
+    QMessageBox::about(this, "About CosmoAI",
+                       "<h2>"
+                       "Test"
+                       "</h2>"
+                       "<br>"
+                       "List:"
+                       "<ul>"
+                       "<li> Item 1 </li>"
+                       "<li> Item 2 </li>"
+                       "<li> Item 3 </li>"
+                       "</ul>"
+                       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                       );
+}
+
