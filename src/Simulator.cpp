de7725376032a5e50b64ce6b609f1db2e1c16e0e
Hans Georg Schaathun <georg@schaathun.net>
2022-09-21 11:27:18 +0200

    Corrected factor in main formula

--- src/Simulator.cpp
+++ src/Simulator.cpp
@@ -164,10 +164,10 @@ std::pair<double, double> Simulator::spherical(double r, double theta) const {
             double beta = betas_val[m][s];
             int c_p = 1 + s/(m + 1);
             int c_m = 1 - s/(m + 1);
-            subTerm1 += 1.0/4*( (alpha*cos((s-1)*theta) + beta*sin((s-1)*theta))*c_p 
-                              + (alpha*cos((s+1)*theta) + beta*sin((s+1)*theta))*c_m );
-            subTerm2 += 1.0/4*( (-alpha*sin((s-1)*theta) + beta*cos((s-1)*theta))*c_p 
-                              + (alpha*sin((s+1)*theta) - beta*cos((s+1)*theta))*c_m);
+            subTerm1 += 0.5*( (alpha*cos((s-1)*theta) + beta*sin((s-1)*theta))*c_p 
+                            + (alpha*cos((s+1)*theta) + beta*sin((s+1)*theta))*c_m );
+            subTerm2 += 0.5*( (-alpha*sin((s-1)*theta) + beta*cos((s-1)*theta))*c_p 
+                            + (alpha*sin((s+1)*theta) - beta*cos((s+1)*theta))*c_m);
         }
         ksi1 += frac*subTerm1;
         ksi2 += frac*subTerm2;
