cd1771755773cfaafba353becef94cd83e64fd23
Hans Georg Schaathun <georg@schaathun.net>
2022-09-21 20:45:07 +0200

    comments

--- src/PointMassSimulator.cpp
+++ src/PointMassSimulator.cpp
@@ -1,3 +1,7 @@
+/* (C) 2022: Hans Georg Schaathun <hg@schaathun.net> */
+/* The point mass model is included in Simulator as a default *
+ * implementation, and this this class overrides nothing.     */
+
 #include "Simulator.h"
 #include <symengine/expression.h>
 #include <symengine/lambda_double.h>
