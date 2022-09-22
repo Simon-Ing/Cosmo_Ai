3be4086fc6d6e8ec7baae3fe77cf8ebc8fe6f60c
Hans Georg Schaathun <georg@schaathun.net>
2022-09-22 20:53:29 +0200

    Data generator compiles.  Not tested.

--- src/Data_generator.cpp
+++ src/Data_generator.cpp
@@ -9,39 +9,48 @@ int main(int, char *argv[]) {
 
     Simulator simulator;
 
+    int nterms = 16 ;
+    int CHI_percent ;
+    int CHI, einsteinR, X, Y, sourceSize ;
+
+
     int DATAPOINTS_TO_GENERATE = atoi(argv[1]);
-    simulator.size = atoi(argv[2]);
-    simulator.name = std::string(argv[3]);
+    int imgsize = atoi(argv[2]);
+    std::string simname = std::string(argv[3]);
     int n_params = atoi(argv[4]);
+
+    simulator = Simulator(imgsize) ;
+
+    double xyrange = imgsize/2.5 ;
 
     // Generate dataset:
     std::random_device dev;
     std::mt19937 rng(dev());
     std::uniform_int_distribution<std::mt19937::result_type> rand_lens_dist(30, 100);
-    std::uniform_int_distribution<std::mt19937::result_type> rand_einsteinR(1, simulator.size/10);
-    std::uniform_int_distribution<std::mt19937::result_type> rand_source_size(1, simulator.size/10);
-    std::uniform_int_distribution<std::mt19937::result_type> rand_xSlider(100, simulator.size-100);
-    std::uniform_int_distribution<std::mt19937::result_type> rand_ySlider(100, simulator.size-100);
+    std::uniform_int_distribution<std::mt19937::result_type> rand_einsteinR(1, imgsize/10);
+    std::uniform_int_distribution<std::mt19937::result_type> rand_source_size(1, imgsize/10);
+    std::uniform_int_distribution<std::mt19937::result_type> rand_xSlider(-xyrange, -xyrange);
+    std::uniform_int_distribution<std::mt19937::result_type> rand_ySlider(-xyrange, -xyrange);
 
     std::vector<std::vector<int>> parameters;
     for (int i = 0; i < DATAPOINTS_TO_GENERATE; i++) {
-        if (n_params == 4){
-            simulator.CHI_percent = 50;
+        if (n_params == 0){
+            CHI_percent = 50;
+        } else {
+            CHI_percent = rand_lens_dist(rng);
         }
-        else{
-            simulator.CHI_percent = rand_lens_dist(rng);
-        }
-        simulator.einsteinR = rand_einsteinR(rng);
-        simulator.sourceSize = rand_source_size(rng);
-        simulator.xPosSlider = rand_xSlider(rng);
-        simulator.yPosSlider = rand_ySlider(rng);
+        CHI = CHI_percent/100 ;
+        einsteinR = rand_einsteinR(rng);
+        sourceSize = rand_source_size(rng);
+        X = rand_xSlider(rng);
+        Y = rand_ySlider(rng);
 
-        std::vector<int> params = {simulator.CHI_percent, simulator.einsteinR, simulator.sourceSize, simulator.xPosSlider, simulator.yPosSlider };
+        std::vector<int> params = {CHI_percent, einsteinR, sourceSize, X, Y };
 
         if ( (!std::count(parameters.begin(), parameters.end(), params)) ) { // check for duplicate
-            simulator.update();
+            simulator.updateAll( X, Y, einsteinR, sourceSize, CHI, nterms );
             simulator.writeToPngFiles(n_params);
-            parameters.push_back({ simulator.CHI_percent, simulator.einsteinR, simulator.sourceSize, simulator.xPosSlider, simulator.yPosSlider });
+            parameters.push_back( params ) ;
         }
         else{
             i--;
