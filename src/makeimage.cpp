b84b1e636d8dcedc16928cb27bd01d0eac340e25
Hans Georg Schaathun <georg@schaathun.net>
2022-09-25 10:20:37 +0200

    Drafted new makeimage executable.  Compiles.  Not tested.

---
+++ src/makeimage.cpp
@@ -0,0 +1,47 @@
+/* (C) 2022: Hans Georg Schaathun <hg@schaathun.net> *
+ * Building on code by Simon Ingebrigtsen, Sondre Westbø Remøy,
+ * Einar Leite Austnes, and Simon Nedreberg Runde
+ */
+
+#include <unistd.h>
+#include <string>
+#include <opencv2/opencv.hpp>
+#include "Simulator.h"
+
+int main(int argc, char *argv[]) {
+
+    Simulator simulator;
+
+    // Set Defaults
+    int nterms = 16 ;
+    int CHI_percent=50 ;
+    int einsteinR=10, X=20, Y=0, sourceSize=10 ;
+    int imgsize = 512 ;
+    double CHI ;
+    std::string simname = "test" ;
+
+    while ( int opt = getopt(argc,argv,"N:x:y:s:n:X:E:I:") > -1 ) {
+       switch(opt) {
+          case 'x': X = atoi(optarg) ; break ;
+          case 'y': Y = atoi(optarg) ; break ;
+          case 's': sourceSize = atoi(optarg) ; break ;
+          case 'X': CHI_percent = atoi(optarg) ; break ;
+          case 'E': einsteinR = atoi(optarg) ; break ;
+          case 'n': nterms = atoi(optarg) ; break ;
+          case 'I': imgsize = atoi(optarg) ; break ;
+          case 'N': simname = optarg ; break ;
+       }
+    }
+
+    CHI = CHI_percent/100.0 ;
+
+    simulator = PointMassSimulator(imgsize) ;
+    simulator.updateAll( X, Y, einsteinR, sourceSize, CHI, nterms );
+
+    std::ostringstream filename;
+    filename << CHI_percent << "," << einsteinR << "," << sourceSize << "," << X << "," << Y << ".png";
+    cv::imwrite( "image- " + simname + filename.str(), simulator.getDistorted());
+    cv::imwrite( "actual- " + simname + filename.str(), simulator.getActual());
+}
+
+
