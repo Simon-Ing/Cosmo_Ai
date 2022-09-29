3d1d0468c121c99103441c97b37dc15b4021068f
Hans Georg Schaathun <georg@schaathun.net>
2022-09-29 21:29:11 +0200

    Allow rotation of ellipsoid source.

--- src/EllipsoidSource.cpp
+++ src/EllipsoidSource.cpp
@@ -3,10 +3,14 @@
 #include "Source.h"
 #include <thread>
 
-EllipsoidSource::EllipsoidSource( int sz, double sig1, double sig2 ) :
-        Source::Source(sz),
+EllipsoidSource::EllipsoidSource( int sz, double sig1, double sig2, double thet ) :
         sigma1(sig1),
-        sigma2(sig2)
+        sigma2(sig2),
+        theta(thet),
+        Source::Source(sz)
+{ }
+EllipsoidSource::EllipsoidSource( int sz, double sig1, double sig2 ) :
+        EllipsoidSource(sz,sig1,sig2,0)
 { }
 
 
@@ -20,5 +24,7 @@ void EllipsoidSource::drawSource(int begin, int end, cv::Mat& dst) {
             dst.at<uchar>(row, col) = value;
         }
     }
+    cv::Mat rot = cv::getRotationMatrix2D( cv::Point(dst.rows/2, dst.cols/2), theta, 1);
+    cv::warpAffine(dst, dst, rot, dst.size());    // crop distorted image
 }
 
