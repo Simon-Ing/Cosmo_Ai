f2ecbdc38eb0b195b02d2e4fd347f6687225c145
Hans Georg Schaathun <georg@schaathun.net>
2022-10-05 12:35:07 +0200

    Added montage

--- datagen.sh
+++ datagen.sh
@@ -6,6 +6,7 @@
 IFS=,
 while read -r idx srcmode lensmode chi x y einsteinr sigma sigma2 theta nterms
 do
-  bin/makeimage -X $chi -x $x -y $y -E $einsteinr -s $sigma -2 $sigma2 -t $theta -n $nterms \
+  bin/makeimage $* -X $chi -x $x -y $y -E $einsteinr -s $sigma -2 $sigma2 -t $theta -n $nterms \
                 -S $srcmode -L $lensmode -N $idx -Z 800
+  convert actual-$idx.png image-$idx.png +append montage$idx.png
 done
