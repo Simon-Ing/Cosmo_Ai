a601a88111a3640725489b63a3792d55ab608dc8
Hans Georg Schaathun <hasc@ntnu.no>
2022-10-30 12:39:17 +0100

    trying to get libraries to work at IDUN

--- config.sh
+++ config.sh
@@ -8,3 +8,11 @@ L='/lib:/lib64:/usr/lib:/usr/lib64'
 export LD_LIBRARY_PATH="$HOME/local/usr/lib:$HOME/local/usr/lib64:$L"
 
 export CPATH=$HOME/local/usr/include:$CPATH
+
+rm -rf build
+CWD=`pwd`
+mkdir -p build
+cd build
+cmake ..
+cd $CWD
+make 
