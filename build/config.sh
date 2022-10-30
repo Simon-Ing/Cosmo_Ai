1d890998ad651e772240a08a21883a9ec58caffe
Hans Georg Schaathun <hasc@ntnu.no>
2022-10-30 10:42:49 +0100

    Draft config for IDUN

---
+++ build/config.sh
@@ -0,0 +1,10 @@
+module purge
+module load intel/2020b
+
+export PATH="$HOME/local/usr/sbin:$HOME/local/usr/bin:$HOME/local/bin:$PATH"
+export MANPATH="$HOME/local/usr/share/man:$MANPATH"
+
+L='/lib:/lib64:/usr/lib:/usr/lib64'
+export LD_LIBRARY_PATH="$HOME/local/usr/lib:$HOME/local/usr/lib64:$L"
+
+export CPATH=$HOME/local/usr/include:$CPATH
