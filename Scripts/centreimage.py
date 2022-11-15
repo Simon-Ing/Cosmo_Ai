5ac1839164096e1a839581ef96bd092ff4d5d1f1
Hans Georg Schaathun <georg@schaathun.net>
2022-11-15 23:36:27 +0100

    Centring seems to work

--- Scripts/centreimage.py
+++ Scripts/centreimage.py
@@ -24,14 +24,42 @@ print("Raw image Shape: ", raw.shape)
 im = cv.cvtColor(raw, cv.COLOR_BGR2GRAY)
 print("Shape converted to grey scale: ", im.shape)
 
-m,n = im.shape
-ps = [ (x,y) for x in range(m) for y in range(n) ]
-s = im.sum()
-xs = [ x*im[x,y] for (x,y) in ps ]
-ys = [ y*im[x,y] for (x,y) in ps ]
-xm = int(round(sum(xs)/s))
-ym = int(round(sum(ys)/s))
+def centreImage(im):
+  m,n = im.shape
+  ps = [ (x,y) for x in range(m) for y in range(n) ]
+  s = im.sum()
+  xs = [ x*im[x,y] for (x,y) in ps ]
+  ys = [ y*im[x,y] for (x,y) in ps ]
+  xm = int(round(sum(xs)/s - m/2))
+  ym = int(round(sum(ys)/s - n/2))
+  
+  print( f"Centre: ({xm},{ym})\n" )
 
-print( f"Centre: ({xm},{ym})\n" )
+  centred = np.zeros( (m,n) )
+  c1 = np.zeros( (m,n) )
 
-# cv.imwrite( "centred-test.png", masked )
+  if xm > 0:
+      c1[:-xm,:] = im[xm:,:]
+  elif xm < 0:
+      c1[-xm:,:] = im[:xm,:]
+  else:
+      c1 = im
+  if ym > 0:
+      centred[:,:-ym] = c1[:,ym:]
+  elif ym < 0:
+      centred[:,-ym:] = c1[:,:ym]
+  else:
+      centred = c1
+  print( f"Range ({centred.min()},{centred.max()})" )
+  return centred
+
+def drawAxes(im):
+  m,n = im.shape
+  im[int(round(m/2)),:] = 127
+  im[:,(round(n/2))] = 127
+  return im
+
+centred = centreImage(im)
+centred = drawAxes(centred)
+
+cv.imwrite( "centred-test.png", centred )
