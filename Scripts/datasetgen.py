92010e4ec6309d5684266db987a4eaf6bd366f30
Hans Georg Schaathun <georg@schaathun.net>
2022-11-18 11:08:26 +0100

    Enforce R>R_E in datagen script

--- Scripts/datasetgen.py
+++ Scripts/datasetgen.py
@@ -4,6 +4,7 @@
 import multiprocessing as mp
 import sys
 import time
+import numpy as np
 
 
 from random import randint
@@ -18,17 +19,30 @@ lensmodes = "pss"
 def getline(idx,chi=0,nterms=16):
     if 0 == chi:
         chi = randint(30,70)
-    x = randint(-75,75)
-    y = randint(-75,75)
-    einsteinR = randint(10,50)
+
+    # Source
     sigma = randint(1,60)
     sigma2 = randint(1,40)
     theta = randint(0,179)
+
+    # Lens
+    einsteinR = randint(10,50)
+
+    # Polar Source Co-ordinates
+    phi = randint(0,359)
+    R = randint(einsteinR,100)
+
+    # Cartesian Co-ordinates
+    x = R*np.cos(np.pi*phi/180)
+    y = R*np.sin(np.pi*phi/180)
+
     srcmode = srcmodes[randint(0,len(srcmodes)-1)]
     lensmode = lensmodes[randint(0,len(lensmodes)-1)]
-    return f'"{idx:04}",image-{idx:04}.png,{srcmode},{lensmode},{chi},{x},{y},{einsteinR},{sigma},{sigma2},{theta},{nterms}'
+    return f'"{idx:04}",image-{idx:04}.png,{srcmode},{lensmode},{chi},"
+         + f"{R},{phi},{einsteinR},{sigma},{sigma2},{theta},{nterms},{x},{y}'
 
-header = "index,filename,source,lens,chi,x,y,einsteinR,sigma,sigma2,theta,nterms\n"
+header = "index,filename,source,lens,chi,"
+       + "R,phi,einsteinR,sigma,sigma2,theta,nterms,x,y\n"
 
 def main():
     with open(fn, 'w') as f:
