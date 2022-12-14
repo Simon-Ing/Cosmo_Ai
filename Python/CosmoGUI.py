0cf161c92e77c94b28b0d183567b3bb07abec494
Hans Georg Schaathun <georg@schaathun.net>
2022-12-14 10:38:55 +0100

    stub

---
+++ Python/CosmoGUI.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+
+"""
+Desktop application to run the CosmoSim simulator for
+gravitational lensing.
+
+(Under construction.)
+"""
+
+from tkinter import *
+from tkinter import ttk
+root = Tk()
+frm = ttk.Frame(root, padding=10)
+frm.grid()
+ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+root.mainloop()
+
