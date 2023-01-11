11f65608ea42ccfa9a9e91cfc63c878168611f62
Hans Georg Schaathun <georg@schaathun.net>
2023-01-11 16:00:04 +0100

    Updated IDUN build setup

--- idunbuild.sh
+++ idunbuild.sh
@@ -6,8 +6,11 @@ module purge
 module load OpenCV/4.5.3-foss-2021a-contrib
 module load CMake/3.20.1-GCCcore-10.3.0
 module load SymEngine/0.7.0-GCC-10.3.0
-mkdir -p bin lib build
-cd build || exit 1
-cmake ..
-cmake --build .
-cmake --install . --prefix ..
+module load Python/3.9.5-GCCcore-10.3.0
+rm -rf build
+mkdir build
+cmake . -B build
+cmake --build build
+
+mkdir -p bin lib 
+cmake --install build --prefix .
