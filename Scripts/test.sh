5b7a26900ce8c7a9be71b8f6e4322838cf54dba1
Hans Georg Schaathun <georg@schaathun.net>
2023-03-20 21:50:19 +0100

    updated test script

--- Scripts/test.sh
+++ Scripts/test.sh
@@ -5,4 +5,4 @@ test $dir || dir=Test/`date "+%Y%m%d"`
 mkdir -p $dir
 
 python3 Python/datagen.py --directory="$dir" --csvfile Datasets/debug.csv  || exit 1
-python3 Python/compare.py Test/v2.0.3 $dir Test/diff
+python3 Python/compare.py --diff Test/diff Test/v2.0.3 $dir
