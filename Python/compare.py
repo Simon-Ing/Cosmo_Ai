bb40da487d16f95b36fae8aa3c8cf71cc381555c
Hans Georg Schaathun <georg@schaathun.net>
2023-03-20 13:06:08 +0100

    Imported test scripts and images from roulettelens. Removed RoulettePMCLens

--- Python/compare.py
+++ Python/compare.py
@@ -14,13 +14,16 @@ import os
 import numpy as np
 import argparse
 
-def comparefiles(f1,f2):
+def comparefiles(f1,f2,fout=None):
     im1 = cv.imread(f1).astype(np.float64)
     im2 = cv.imread(f2).astype(np.float64)
     diff = im1 - im2
     print( f"{f1}: "
          + f"Pixel diff: max={np.max(diff)}/min={np.min(diff)}; " 
          + f"Euclidean distance {cv.norm(diff)}" )
+    dfim = (diff+256)/2
+    if fout != None:
+        cv.imwrite( fout, dfim ) 
 
 
 if __name__ == "__main__":
@@ -29,11 +32,17 @@ if __name__ == "__main__":
           description = 'Compare images for debugging purposes',
           epilog = '')
 
+    parser.add_argument('-f', '--files', action='store_true', 
+            help="Compare individual files")
     parser.add_argument('dir1')
     parser.add_argument('dir2')
+    parser.add_argument('dir3')
     args = parser.parse_args()
 
-    for fn in os.listdir(args.dir1):
+    if args.files:
+        comparefiles(args.dir1,args.dir2,args.dir3)
+    else:
+      for fn in os.listdir(args.dir1):
         f1 = os.path.join(args.dir1,fn)
         if not os.path.isfile(f1): 
             print( "Not a file: ", f1 )
@@ -42,5 +51,9 @@ if __name__ == "__main__":
         if not os.path.isfile(f2): 
             print( "Not a file: ", f2 )
             break
-        comparefiles(f1,f2)
+        if args.dir3 == None:
+            f3 = None
+        else:
+            f3 = os.path.join(args.dir3,fn)
+        comparefiles(f1,f2,f3)
 
