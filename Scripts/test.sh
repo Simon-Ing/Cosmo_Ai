19375ee4fad00db1606a513aff03dfa595d1499e
Hans Georg Schaathun <georg@schaathun.net>
2023-03-20 14:16:13 +0100

    exit clause in test script

--- Scripts/test.sh
+++ Scripts/test.sh
@@ -4,5 +4,5 @@ dir=$1
 test $dir || dir=Test/`date "+%Y%m%d"`
 mkdir -p $dir
 
-python3 Python/datagen.py --directory="$dir" --csvfile Datasets/debug.csv 
+python3 Python/datagen.py --directory="$dir" --csvfile Datasets/debug.csv  || exit 1
 python3 Python/compare.py Test/v2.0.2 $dir Test/diff
