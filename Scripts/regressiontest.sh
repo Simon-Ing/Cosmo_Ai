0bd47793391a92b7ed52d88c16bf6043ab5a936c
Hans Georg Schaathun <georg@schaathun.net>
2023-03-22 13:34:23 +0100

    Drafted new debug dataset and regression script

--- Scripts/regressiontest.sh
+++ Scripts/regressiontest.sh
@@ -7,5 +7,25 @@ mkdir -p $dir
 baseline=Test/baseline20230320
 # baseline=Test/v2.0.3
 
-python3 Python/datagen.py --directory="$dir" --csvfile Datasets/debug.csv  || exit 1
-python3 Python/compare.py --diff Test/diff $baseline $dir
+F=mask centred reflines
+
+
+for flag in $F plain 
+do
+   mkdir -p $dir/$flag
+   mkdir -p Test/diff/$flag
+done
+
+python3 Python/datagen.py --directory="$dir"/plain --csvfile Datasets/debug.csv  || exit 1
+
+for flag in $F
+do
+  python3 Python/datagen.py --$flag --directory="$dir"/$flag --csvfile Datasets/debug.csv  || exit 2
+done
+
+test -d $baseline || exit 3
+
+for flag in $F plain 
+do
+   python3 Python/compare.py --diff Test/diff/$flag $baseline/$flag $dir/$flag
+done
