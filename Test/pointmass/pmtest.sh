10b828f34993454f5afc51bcc489458eb04aa2d6
Hans Georg Schaathun <georg@schaathun.net>
2023-03-22 18:51:12 +0100

    Drafting pmtest

---
+++ Test/pointmass/pmtest.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+pdir=../../Python
+dir1=Exact
+dir2=Roulette
+diffdir=diff
+
+mkdir -p $dir1 $dir2 $diffdir
+
+fn=../sphere.csv
+
+python3 $pdir/datagen.py -L p --directory="$dir1" --csvfile $fn 
+python3 $pdir/datagen.py -L r --directory="$dir2" --csvfile $fn 
+python3 $pdir/compare.py --diff $diffdir $dir1 $dir2
+
