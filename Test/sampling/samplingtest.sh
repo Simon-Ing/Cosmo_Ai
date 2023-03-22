0bf675a596301e3ae62b9b0a092d450db78a4ed9
Hans Georg Schaathun <georg@schaathun.net>
2023-03-22 19:39:03 +0100

    Sampled SIS looks great

--- Test/sampling/samplingtest.sh
+++ Test/sampling/samplingtest.sh
@@ -9,7 +9,7 @@ mkdir -p $dir1 $dir2 $diffdir
 
 fn=../spheres.csv
 
-python3 $pdir/datagen.py -L s --directory="$dir1" --csvfile $fn 
+python3 $pdir/datagen.py -L ss --directory="$dir1" --csvfile $fn 
 python3 $pdir/datagen.py -L sr --directory="$dir2" --csvfile $fn 
 python3 $pdir/compare.py --diff $diffdir $dir1 $dir2
 
