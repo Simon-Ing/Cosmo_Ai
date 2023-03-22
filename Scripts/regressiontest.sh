98cbde3e91869d2ee6a0e23f462c8285b3f9aec2
Hans Georg Schaathun <georg@schaathun.net>
2023-03-22 13:18:28 +0100

    Changed main test script

--- Scripts/regressiontest.sh
+++ Scripts/regressiontest.sh
@@ -4,7 +4,8 @@ dir=$1
 test $dir || dir=Test/`date "+%Y%m%d"`
 mkdir -p $dir
 
-baseline=baseline20230320
+baseline=Test/baseline20230320
+# baseline=Test/v2.0.3
 
 python3 Python/datagen.py --directory="$dir" --csvfile Datasets/debug.csv  || exit 1
 python3 Python/compare.py --diff Test/diff $baseline $dir
