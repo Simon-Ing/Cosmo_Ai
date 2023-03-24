d886570f9ffc407eff33eb6ef575136938dabb1f
Hans Georg Schaathun <georg@schaathun.net>
2023-03-24 12:43:44 +0100

    More test images

--- Scripts/currenttest.sh
+++ Scripts/currenttest.sh
@@ -16,6 +16,9 @@ index,filename,source,chi,x,y,einsteinR,sigma,sigma2,theta,nterms
 "s04",image-s04.png,e,50,-38,28,21,5,15,75,50
 "s07",image-s07.png,s,50,-20,-20,10,15,0,0,50
 "s08",image-s08.png,s,50,30,0,12,15,0,0,50
+"s11",image-s01.png,s,50,30,30,40,20,0,0,50
+"s12",image-s02.png,s,50,35,24,30,30,0,0,50
+"s13",image-s02.png,s,50,40,40,60,20,0,0,50
 EOF
 
 python3 Python/datagen.py -L sr --directory="$dir" --csvfile $fn --actual --apparent --family --reflines
