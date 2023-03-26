58cb2e725d1366632157dde927347d59b594c1fa
Hans Georg Schaathun <georg@schaathun.net>
2023-03-26 16:30:00 +0200

    Drafted new SIS and Lens classes.

--- include/cosmosim/Lens.h
+++ include/cosmosim/Lens.h
@@ -12,5 +12,15 @@ public:
     virtual void updatePsi() ;
 };
 
+class SIS : public Lens { 
+
+private:
+
+public:
+    virtual double psifunction( double, double ) = 0 ;
+    virtual double psiXfunction( double, double ) = 0 ;
+    virtual double psiYfunction( double, double ) = 0 ;
+};
+
 
 #endif // LENS_H
---
+++ src/simlib/SIS.cpp
@@ -0,0 +1,17 @@
+/* (C) 2023: Hans Georg Schaathun <georg@schaathun.net> */
+
+#include "cosmosim/PixMap.h"
+#include "simaux.h"
+
+double SIS::psifunction( double x, double y ) {
+   return einsteinR*sqrt( x*x + y*y ) ;
+}
+double SIS::psiXfunction( double x, double y ) {
+   double s = sqrt( x*x + y*y ) ;
+   return einsteinR*x*s ;
+}
+double SIS::psiYfunction( double x, double y ) {
+   double s = sqrt( x*x + y*y ) ;
+   return einsteinR*y*s ;
+}
+
