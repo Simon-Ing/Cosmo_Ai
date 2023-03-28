3ff830d8310d3e2b9aa1556427668452fcc34355
Hans Georg Schaathun <hasc@ntnu.no>
2023-03-28 13:30:42 +0200

    New test batch for IDUN

--- idunbuild.sh
+++ idunbuild.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 # (C) 2022: Hans Georg Schaathun <georg@schaathun.net> 
 
 # Build scripts for NTNU's IDUN cluster.
@@ -7,6 +8,8 @@ module load OpenCV/4.5.3-foss-2021a-contrib
 module load CMake/3.20.1-GCCcore-10.3.0
 module load SymEngine/0.7.0-GCC-10.3.0
 module load Python/3.9.5-GCCcore-10.3.0
+module list
+
 rm -rf build
 mkdir build
 cmake . -B build
