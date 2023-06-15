8f2d763b0788799eb841c77d691b64862ac41ae9
Hans Georg Schaathun <georg@schaathun.net>
2023-06-15 18:12:50 +0200

    More plots in test script

--- Scripts/allmodels.sh
+++ Scripts/allmodels.sh
@@ -1,6 +1,13 @@
 #! /bin/sh
 
+sh build.sh
+
 python3 Python/datagen.py -x 10 -y 50 --model Raytrace --lens SIS --name raytrace -R
 python3 Python/datagen.py -x 10 -y 50 --model Roulette --lens SIS --name roulette -R
 python3 Python/datagen.py -x 10 -y 50 --model Raytrace --lens SIS --sampled --name raytrace-sampled -R
 python3 Python/datagen.py -x 10 -y 50 --model Roulette --lens SIS --sampled --name roulette-sampled -R
+
+python3 Python/datagen.py -x 50 -y 0 --model Raytrace --lens SIS --name raytrace2 -R
+python3 Python/datagen.py -x 50 -y 0 --model Roulette --lens SIS --name roulette2 -R
+python3 Python/datagen.py -x 50 -y 0 --model Raytrace --lens SIS --sampled --name raytrace-sampled2 -R
+python3 Python/datagen.py -x 50 -y 0 --model Roulette --lens SIS --sampled --name roulette-sampled2 -R
