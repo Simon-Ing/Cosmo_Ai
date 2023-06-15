550192b296786fe4e44403b30d58e17efd840728
Hans Georg Schaathun <georg@schaathun.net>
2023-06-15 14:44:49 +0200

    Fixed the new test script

--- Scripts/allmodels.sh
+++ Scripts/allmodels.sh
@@ -1,7 +1,6 @@
 #! /bin/sh
 
-for model in p r s ss pss fs rs
-do
-    python3 Python/datagen.py -x 10 -y 50 --lens $model --name $model
-done
-
+python3 Python/datagen.py -x 10 -y 50 --model Raytrace --lens SIS --name raytrace
+python3 Python/datagen.py -x 10 -y 50 --model Roulette --lens SIS --name roulette
+python3 Python/datagen.py -x 10 -y 50 --model Raytrace --lens SIS --sampled --name raytrace-sampled
+python3 Python/datagen.py -x 10 -y 50 --model Roulette --lens SIS --sampled --name roulette-sampled
