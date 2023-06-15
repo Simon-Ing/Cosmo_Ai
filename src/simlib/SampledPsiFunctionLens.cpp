be4fd2f9a1ef0f536e95409f74b64eb0cc0b462c
Hans Georg Schaathun <georg@schaathun.net>
2023-06-15 16:30:20 +0200

    Removed superfluous code

--- src/simlib/SampledPsiFunctionLens.cpp
+++ src/simlib/SampledPsiFunctionLens.cpp
@@ -11,16 +11,10 @@ void SampledPsiFunctionLens::setEinsteinR( double r ) {
 }
 
 void SampledPsiFunctionLens::updatePsi( cv::Size size ) { 
-   // cv::Mat im = getApparent() ;
-   int nrows = size.height ;
-   int ncols = size.width ;
-
    std::cout << "[SampledPsiFunctionLens] updatePsi " << size << "\n" ;
 
    lens->updatePsi(size) ;
-
    psi = lens->getPsi() ;
-
    gradient( -psi, psiX, psiY ) ;
 
    return ; 
