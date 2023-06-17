81f28ad0ea8da85e273d4313616e1bbeca795433
Hans Georg Schaathun <georg@schaathun.net>
2023-06-17 08:46:33 +0200

    Test script

--- Test/roulettelens/test.sh
+++ Test/roulettelens/test.sh
@@ -1,4 +1,6 @@
+#!/bin/sh
 
+export PATH=../../Python:$PATH
 
-python3 datagen.py --csvfile debug.csv --outfile roulette.csv --model Raytrace --centred
-python3 roulettegen.py --csvfile roulette.csv --model Roulette --centred
+python3 datagen.py --csvfile debug.csv --outfile roulette.csv --model Raytrace --centred -D Original
+python3 roulettegen.py --csvfile roulette.csv --model Roulette --centred -D Roulette
