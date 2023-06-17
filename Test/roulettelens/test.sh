e99e7f4581feddafda980fb2bacadd882c0ba603
Hans Georg Schaathun <georg@schaathun.net>
2023-06-17 09:34:14 +0200

    Debugging.  Not done.

--- Test/roulettelens/test.sh
+++ Test/roulettelens/test.sh
@@ -2,5 +2,5 @@
 
 pyd=../../Python
 
-python3 $pyd/datagen.py --csvfile debug.csv --outfile roulette.csv --model Raytrace --centred -D Original &&\
+python3 $pyd/datagen.py --csvfile debug.csv --outfile roulette.csv --centred -D Original &&\
 python3 $pyd/roulettegen.py --csvfile roulette.csv --model Roulette --centred -D Roulette
