a5d2b5ce949bf27e7f56a7f4d351d84f6ec03e70
Hans Georg Schaathun <georg@schaathun.net>
2023-06-23 13:17:31 +0200

    Added diff to the test script

--- Test/roulettelens/test.sh
+++ Test/roulettelens/test.sh
@@ -2,5 +2,8 @@
 
 pyd=../../Python
 
-python3 $pyd/datagen.py --csvfile debug.csv --outfile roulette.csv --centred -D Original &&\
-python3 $pyd/roulettegen.py --csvfile roulette.csv --model Roulette --centred -D Roulette --lens Roulette
+mkdir -p Original Roulette diff
+
+python3 $pyd/datagen.py --csvfile debug.csv --outfile roulette.csv --centred -D Original || exit 1
+python3 $pyd/roulettegen.py --csvfile roulette.csv --model Roulette --centred -D Roulette --lens Roulette || exit 2
+python3 $pyd/compare.py --diff diff/ Original Roulette
