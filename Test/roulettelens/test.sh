dfc5dd36fb1031625debdebc1f0cab6d8909122e
Hans Georg Schaathun <georg@schaathun.net>
2023-06-28 08:04:12 +0200

    montage images from test script.  The resimulated image seems 90° rotated.

--- Test/roulettelens/test.sh
+++ Test/roulettelens/test.sh
@@ -2,8 +2,15 @@
 
 pyd=../../Python
 
-mkdir -p Original Roulette diff
+mkdir -p Original Roulette diff montage
 
-python3 $pyd/datagen.py -Z 600 --csvfile debug.csv --outfile roulette.csv --centred -D Original | tee datagen.log || exit 1
+python3 $pyd/datagen.py -Z 600 --csvfile debug.csv --outfile roulette.csv --centred -D Original --actual | tee datagen.log || exit 1
 python3 $pyd/roulettegen.py -n 10 -Z 600 --csvfile roulette.csv --model Roulette --centred -D Roulette --lens Roulette | tee roulettegen.log || exit 2
 python3 $pyd/compare.py --diff diff/ Original Roulette
+
+for i in diff/*
+do
+   f=`basename $i`
+   convert \( Original/actual-$f Original/$f +append \) \( diff/$f Roulette/$f +append \) -append montage/$f
+done
+
