308f65ac34ed3b293b66461409b811a73fb27bab
Hans Georg Schaathun <georg@schaathun.net>
2023-07-02 08:02:48 +0200

    Improved output from compare.py

--- Python/compare.py
+++ Python/compare.py
@@ -10,12 +10,17 @@ def comparefiles(f1,f2,fout=None):
     im1 = cv.imread(f1).astype(np.float64)
     im2 = cv.imread(f2).astype(np.float64)
     diff = im1 - im2
+    norm = cv.norm(diff)
+    relnorm = norm/diff.size
+    mx = np.max(diff)
+    mn = np.min(diff)
     print( f"{f1}: "
-         + f"Pixel diff: max={np.max(diff)}/min={np.min(diff)}; " 
-         + f"Euclidean distance {cv.norm(diff)}" )
+         + f"Pixel diff: max={mx}/min={mn}; " 
+         + f"Euclidean distance {norm} or {relnorm} per pizel." )
     dfim = (diff+256)/2
     if fout != None:
         cv.imwrite( fout, dfim ) 
+    return (f1,relnorm,norm,mn,mx)
 
 
 if __name__ == "__main__":
@@ -34,6 +39,7 @@ if __name__ == "__main__":
     if args.files:
         comparefiles(args.dir1,args.dir2,args.diff)
     else:
+      results = []
       for fn in os.listdir(args.dir1):
         f1 = os.path.join(args.dir1,fn)
         f2 = os.path.join(args.dir2,fn)
@@ -46,5 +52,10 @@ if __name__ == "__main__":
         elif not os.path.isfile(f2): 
             print( "Not a file: ", f2 )
         else:
-            comparefiles(f1,f2,f3)
+            results.append( comparefiles(f1,f2,f3) )
+      badresults = [ r for r in results if r[2] > 0 ]
+      badresults.sort(key=lambda x : x[1])
+      print( "Bad results sorted by distance:" )
+      for r in badresults:
+          print( f"{r[0]}: {r[1]} ({r[2]}) range {(r[3],r[4])}" )
 
