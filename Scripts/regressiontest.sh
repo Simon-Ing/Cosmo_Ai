db2a895fba86a980dc2137ee80671479ff4487aa
Hans Georg Schaathun <georg@schaathun.net>
2023-07-04 08:48:12 +0200

    Improved the regression test to store results as workflow artifacts and avoid some issues on Windows. (Imported from bug/macos)

--- Scripts/regressiontest.sh
+++ Scripts/regressiontest.sh
@@ -1,6 +1,9 @@
 #! /bin/sh
 
-cmake --build build || exit 10
+if test x$COSMOSIM_DEVELOPING = xyes
+then
+   cmake --build build || exit 10
+fi
 
 dir=$1
 test $dir || dir=Test/`date "+%Y%m%d"`
@@ -8,7 +11,6 @@ mkdir -p $dir
 
 baseline=$2
 test $baseline || baseline=Test/baseline20230703
-# baseline=Test/v2.0.3
 
 F=mask
 # F="mask centred reflines"
@@ -37,21 +39,32 @@ python3 CosmoSimPy/datagen.py --reflines --directory="$dir"/reflines \
    --csvfile Datasets/debug.csv  || exit 4
 fi
 
-if test -d $baseline
+if test -d $baseline 
 then 
+  if test -x "`which convert`"
+  then
 
-  for flag in $F plain 
-  do
-     echo $flag
-     python3 CosmoSimPy/compare.py --diff Test/diff/$flag $baseline/$flag $dir/$flag --masked
-     for f in Test/diff/$flag/*
-     do
-        ff=`basename $f`
-        convert $baseline/$flag/$ff Test/diff/$flag/$ff $dir/$flag/$ff +append Test/montage/$flag/$ff
-     done
-  done
+    for flag in $F plain 
+    do
+       echo $flag
+       python3 CosmoSimPy/compare.py --diff Test/diff/$flag \
+          $baseline/$flag $dir/$flag --masked
+       for f in Test/diff/$flag/*
+       do
+          ff=`basename $f`
+          echo "$ff" - "$f"
+          convert $baseline/$flag/$ff Test/diff/$flag/$ff $dir/$flag/$ff  \
+              +append Test/montage/$flag/$ff
+       done
+    done
 
-  echo $F
+    echo $F
+  else
+     echo basename test: `basename /foo/bar/test/image.png` \
+          "(basename /foo/bar/test/image.png)"
+     echo ImageMagick is not installed 
+     exit 6 
+  fi
 else
    echo $baseline does not exist 
    exit 5 
