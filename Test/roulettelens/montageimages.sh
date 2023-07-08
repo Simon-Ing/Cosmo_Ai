763e11515a7ec47bb146f47df05c32a2bba4d4d4
Hans Georg Schaathun <georg@schaathun.net>
2023-07-08 11:13:46 +0200

    Redrafting the roulette regeneration test as makefiles

---
+++ Test/roulettelens/montageimages.sh
@@ -0,0 +1,38 @@
+#! /bin/sh
+
+echo Running: $0
+bindir=`dirname $0`
+echo Running from $bindir
+
+. $bindir/config.sh
+
+dir=$1
+test $dir || dir=`date "+%Y%m%d"`
+mkdir -p montage
+
+baseline=Original
+dir=Roulette
+
+if test -z "$CONVERT"
+then
+     echo ImageMagick is not installed 
+     exit 6 
+elif test ! -d $baseline 
+then 
+   echo $baseline does not exist 
+   exit 5 
+elif test ! -d $dir 
+then 
+   echo Images have not been 
+   exit 5 
+elif test ! -d diff
+then 
+   echo Difference images have not been 
+   exit 5 
+else
+    for f in diff/*
+    do
+          ff=`basename $f`
+          $CONVERT $baseline/$ff diff/$ff $dir/$ff  +append montage/$ff
+    done
+fi
