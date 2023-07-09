19dbbf6ee2aa77ecf48646a9444fde7fc7bcfdd0
Hans Georg Schaathun <georg@schaathun.net>
2023-07-09 07:21:07 +0200

    Hack to make workflow work on Windows

--- RegressionTest/bin/makeimages.sh
+++ RegressionTest/bin/makeimages.sh
@@ -4,6 +4,12 @@ echo Running: $0
 bindir=`dirname $0`
 echo Running from $bindir
 
+if test -f ./makeimages.sh
+then
+   echo Starting ./makeimages.sh
+   exec sh ./makeimages.sh
+fi
+
 . $bindir/config.sh
 
 dir=$1
